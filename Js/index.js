1fc4d41f0f9ceb991b552bcdc33f7210e9fb0723
kando-o <64841289+kando-o@users.noreply.github.com>
2023-07-26 22:59:49 +0200

    Projet-7 add counterCard & onFilter

--- Js/index.js
+++ Js/index.js
@@ -1,9 +1,10 @@
-import {cardsCounter }  from "./Components/counter.js";
+import {cardsCounter } from "./Components/counter.js";
 import { getData } from "./Api/api.js";
 import Card from "./Model/card.js";
 import Tags from "./Components/tags.js";
 import Search from "./Components/search.js";
 
+// For the dropdown
 $(document).ready(function() {
     $('.js-example-basic-single').select2({
 		width : 'resolve'
@@ -20,21 +21,46 @@ window.onload = () => {
 		const cards = recipes.map( recipe => {
 			
 			const card = new Card(recipe)
-			
-			card.name = ""
 			card.buildHtml()
 			parent.appendChild(card.element)
-			
 			return card
 		})
 		
-		// Conteur de carte
 		cardsCounter(cards)
 		
 		const search = new Search(cards)
-		const tagsglobal = new Tags(cards)
+		const tags = new Tags(cards)
 
-		search.searchGlobal()
-		tagsglobal.builgTagsHtml()
+		/**
+		 * 
+		 * @param {string} filtered | Value of filteredByTags
+		 */
+		const displayCards = (filtered) => {
+			if (cards.length === filtered.length) {
+				
+				cards.forEach(el => el.element.classList.remove('hidden'))
+				
+			} else {
+				cards.forEach(el => el.element.classList.add('hidden'))
+				filtered.forEach(el => el.element.classList.remove('hidden'))
+			}
+		}
+
+		const onFilter = () => {
+			const input = document.querySelector('.topPage__inputSearch').value
+			const filtered = search.matchInputUser(input)
+			const filteredByTags = tags.matchTags(filtered)
+			const filteredByInputAndTags = tags.matchInputTags(filteredByTags)
+
+			displayCards(filteredByTags)
+			tags.displayTags(filteredByTags)
+			tags.displayTags(filteredByInputAndTags)
+		}
+
+		search.searchGlobal(onFilter)
+		tags.builgTagsHtml()
+		tags.eventTag(onFilter)
+
+		onFilter()
 	})		
 }
