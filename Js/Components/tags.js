aea2bd8b544e7896fdc1f60684d85998104ddf85
kando-o <64841289+kando-o@users.noreply.github.com>
2023-07-26 23:05:08 +0200

    Projet-7 refacto function builgTagsHtml & add function eventTag displayTags. Function matchTags & matchInputTags in process

--- Js/Components/tags.js
+++ Js/Components/tags.js
@@ -1,38 +1,111 @@
-export default class Tags {
+class Tags {
 	
 	constructor (cards) {
 
 		this.cards = cards;
 
 		const set = new Set();
-		cards.forEach(element =>  element.ingredients.forEach(i => set.add(i._name)));
-		this.ingredient = Array.from(set)
+		cards.forEach(element =>  element.ingredients.forEach(i => set.add(i.name.toLowerCase())));
+		this.ingredients = Array.from(set)
 
 		const setAppareils = new Set();
-		cards.forEach(element =>  element.ingredients.forEach(i => setAppareils.add(i._name)));
+		cards.forEach(element =>  element.appliances.forEach(el=>setAppareils.add(el.toLowerCase())));
 		this.appareils = Array.from(setAppareils)
+
+		const setUstensils = new Set();
+		cards.forEach(element => element.ustensils.forEach(el=>setUstensils.add(el.toLowerCase())))
+		this.ustensils = Array.from(setUstensils)
+
 	}
 
 	builgTagsHtml () {
 
-		// const arrayIngredient = this.ingredient
 		const ingredient = document.getElementById('ingredients')
-		const appareils = document.getElementById('appareils')
-		const ustensile = document.getElementById('ustensiles')
+		const appareil = document.getElementById('appareils')
+		const ustensil = document.getElementById('ustensiles')
+		const recettesHeaderTag = document.querySelector('.recettes__headerTag')
+		const containerTagsDynamique = document.createElement('div')
 
-		this.ingredient.forEach(el => ingredient.innerHTML += ` <option class="ingredients" value="ingredients ">${el}</option> ` )
-		this.appareils.forEach(el => appareil.innerHTML += ` <option class="ingredients" value="ingredients ">${el}</option> ` )
-		this.ingredient.forEach(el => ustensil.innerHTML += ` <option class="ingredients" value="ingredients ">${el}</option> ` )
+		containerTagsDynamique.classList.add('recettes__containerTagsDynamique')
+		recettesHeaderTag.appendChild(containerTagsDynamique)
 		
-		console.log(this.cards, );
+
+
+		ingredient.innerHTML = this.ingredients.reduce((text, el) => text += `<option class="menu-item" value="ingredients ">${el}</option>`, "" )
+		appareil.innerHTML = this.appareils.reduce((text, el) => text += `<option class="menu-item" value="appareils ">${el}</option>`, "" )
+		ustensil.innerHTML = this.ustensils.reduce((text, el) => text += `<option class="menu-item" value="ustensils ">${el}</option>`, "" )
 	}
 
+	eventTag (onFilter) {
 
-	
+		// TODO : create tags on click
+		// TODO : add cross to close the tag
+		// TODO : add event listener to call filter when tag is add
+		// TODO : add event listener to call filter when tag is removed
+
+		const test = [...document.querySelectorAll('.recettes__itemsTag ' )]
+		const tagUser = test.forEach(el => {
+			el.onchange = (e) => {
+				console.log("->"+el.options[el.selectedIndex].text );
+				const selectTags = el.options[el.selectedIndex].text
+				
+				const tagFilter = this.cards.filter(card => {
+					return card._appliances.find(a => a.toLowerCase().includes(selectTags)) ||
+					card._ingredients.find(i => i.ingredient.toLowerCase().includes(selectTags)) ||
+					card._ustensils.find(u => u.toLowerCase().includes(selectTags)) 
+				})
+
+				onFilter()
+				
+				console.log(tagFilter);
+				
+				return el.options[el.selectedIndex].text 			
+			}
+		})
+
+		// TODO : Refaire la liste des tags disponible dans le dropdown avec les cards restant
+		
+		console.log(tagUser);
+		
+		const tagMatch = (cards) => {
+			// const that = this
+			cards = this.cards
+			
+			return cards;
+		}
+
+		console.log( tagMatch(this.cards));
+
+		// si element choisi dans la liste de tag correspond à un element de ingredient ou apparel ou ustensil d'une des cartes alors lance moi la fonction matchCard
+		
+	}
+
+	displayTags (cards) {
+		// traverse all cards
+		// hide tags not in cards
+	}
+
+	matchTags (filtered) {
+		// console.log(tagUser);
+
+
+		// au clique sur un tag ajoute se tag dans la container tags 
+		// au clique sur la croix rouge remove element => tags
+		// si filtered == true alors lance filtered by search & tags
+		// sinon lance juste matchtags
+		// TODO : filter by tags
+		return filtered
+	}
+
+	matchInputTags (filtered) {
+
+		return filtered
+
+	}
 }
-
-// Utiliser la délégation d'event pour les diffs tags
 
 // ingredient 
 // appareils
 // Ustensils
+
+export default Tags
