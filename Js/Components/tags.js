90ad218233de7342725465e7b5c74d0b23acaf06
kando-o <64841289+kando-o@users.noreply.github.com>
2023-09-08 16:27:00 +0200

    Projet-7 change to method for & add svg in input dropdown

--- Js/Components/tags.js
+++ Js/Components/tags.js
@@ -5,16 +5,25 @@ class Tags {
 		this.cards = cards;
 
 		const set = new Set();
-		cards.forEach(element =>  element.ingredients.forEach(i => set.add(i.name.toLowerCase())));
-		this.ingredients = Array.from(set)
+		for (const element of cards) {
+		for (const i of element.ingredients) {
+			set.add(i.name.toLowerCase());
+		}}
+		this.ingredients = Array.from(set);
 
 		const setAppareils = new Set();
-		cards.forEach(element =>  element.appliances.forEach(el=>setAppareils.add(el.toLowerCase())));
-		this.appareils = Array.from(setAppareils)
+		for (const element of cards) {
+		for (const el of element.appliances) {
+			setAppareils.add(el.toLowerCase());
+		}}
+		this.appareils = Array.from(setAppareils);
 
 		const setUstensils = new Set();
-		cards.forEach(element => element.ustensils.forEach(el=>setUstensils.add(el.toLowerCase())))
-		this.ustensils = Array.from(setUstensils)
+		for (const element of cards) {
+		for (const el of element.ustensils) {
+			setUstensils.add(el.toLowerCase());
+		}}
+		this.ustensils = Array.from(setUstensils);
 	}
 
 	builgTagsHtml () {
@@ -24,35 +33,89 @@ class Tags {
 		const ustensil = document.getElementById('ustensiles')
 		const recettesHeaderTag = document.querySelector('.recettes__headerTag')
 		
-		const containerTagsDynamique = document.createElement('div')
-		containerTagsDynamique.classList.add('recettes__containerTagsDynamique')
+		const containerTags = document.createElement('div')
+		containerTags.classList.add('recettes__containerTags')
+		this.tagsContainer = containerTags
 		
-		this.tagsContainer = containerTagsDynamique
-		const btnlabelClose = document.createElement('div')
-		btnlabelClose.classList.add('.testTag')
-		
+		// creat container for svg
+		const containerSvg = document.createElement('div')
+		containerSvg.classList.add('containerSvg')
 
-		recettesHeaderTag.appendChild(containerTagsDynamique)
+		// create svg search
+		const search = document.createElement('svg')
+		search.innerHTML = ` <svg class="containerSvg__item search" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<circle cx="5" cy="5" r="4.75" stroke="#7A7A7A" stroke-width="0.5"/>
+		<line x1="9.17678" y1="9.32322" x2="13.6768" y2="13.8232" stroke="#7A7A7A" stroke-width="0.5"/>
+		</svg>
+		`
+		// create svp close
+		const close = document.createElement('span')
+		close.innerHTML = `<span class="containerSvg__item close"> X </span>`
 		
-		ingredient.innerHTML = this.ingredients.reduce((text, el) => text += `<option class="menu-item" value="ingredients ">${el}</option>`, "" )
-		appareil.innerHTML = this.appareils.reduce((text, el) => text += `<option class="menu-item" value="appareils ">${el}</option>`, "" )
-		ustensil.innerHTML = this.ustensils.reduce((text, el) => text += `<option class="menu-item" value="ustensils ">${el}</option>`, "" )
+		// add svg in dropdown
+		const select2Selection = document.querySelectorAll('.select2-selection')
+		select2Selection.forEach( el => {
+				el.addEventListener('click', () => {
+					const select2Search = document.querySelector('.select2-search')
+					const select2Field = document.querySelectorAll('.select2-search ')
+					let buttonClose 
+
+					containerSvg.appendChild(close)
+					containerSvg.appendChild(search)
+					select2Search.appendChild(containerSvg)
+					
+					if (select2Search) {
+						
+						setTimeout(	() => {
+							buttonClose = select2Search.querySelector('.close')
+							if (buttonClose) {
+								buttonClose.addEventListener('click', () => {									
+									select2Search.querySelector('input').value = ''  
+									ingredient.innerHTML = ingredientOptions;
+								})
+							}
+						}, 1000)
+					}
+				})
+			}
+		)
+
+		recettesHeaderTag.appendChild(containerTags)
+		
+		let ingredientOptions = "";
+		for (const el of this.ingredients) {
+			ingredientOptions += `<option class="menu-item" value="ingredients">${el}</option>`;
+		}
+		ingredient.innerHTML = ingredientOptions;
+
+		let appareilOptions = "";
+		for (const el of this.appareils) {
+			appareilOptions += `<option class="menu-item" value="appareils">${el}</option>`;
+		}
+		appareil.innerHTML = appareilOptions;
+
+		let ustensilOptions = "";
+		for (const el of this.ustensils) {
+			ustensilOptions += `<option class="menu-item" value="ustensils">${el}</option>`;
+		}
+		ustensil.innerHTML = ustensilOptions;	
 	}
 
 	listeners (onFilter) {
 
-		const test = [...document.querySelectorAll('.recettes__itemsTag ' )]
-		
-		test.forEach(el => {
+		const itemsTag = [...document.querySelectorAll('.recettes__itemsTag')]
+		for (const el of itemsTag) {
 			const type = el.getAttribute('id')
+			el.onchange = (e) => {
+				
+				const text = el.options[el.selectedIndex].text;
+				const existingTags = this.tagsContainer.querySelectorAll(".tag-text");
+				for (const tag of existingTags) {
+					if (tag.textContent === text) return;
+				}
 
-			el.oninput = (e) => {
-				
-				const text = el.options[el.selectedIndex].text
-				if ([...this.tagsContainer.querySelectorAll(".tag-text")].find(t=>t.textContent===text)) return
-				
-				const currentTag = document.createElement("div")
-				currentTag.classList.add("recettes__labelTag")
+				const currentTag = document.createElement("div");
+				currentTag.classList.add("recettes__labelTag");
 
 				const tagText = document.createElement("span")
 				tagText.textContent = text
@@ -73,35 +136,90 @@ class Tags {
 
 				onFilter()
 			}
-		})
-
+		}
 	}
 
-	display (cards) {
-		// hide all tags in selects
-		// traverse all cards
-		// hide tags not in cards
-	}
-
-	match (cards) {
-
-		if (!cards || cards.length<1) return cards
+	match(cards) {
+		
+		if (!cards || cards.length < 1) return cards;
 	
-		const tags = [...this.tagsContainer.querySelectorAll(".tag-text")]
-		if (tags.length<1) return cards
+		const tags = [...this.tagsContainer.querySelectorAll(".tag-text")];
+		if (tags.length < 1) return cards;
+		
+		// log for the tag selected
+		for (const tag of tags) {
+		console.log('getAttribute -->'+tag.getAttribute("tag-type"), 'tag+textContent -->'+tag.textContent);
+		}
+	
+		const filteredCards = [];
 
-		tags.forEach(t=>console.log(t.getAttribute("tag-type"), t.textContent))
-		return cards.filter(card => {
-			return tags.filter( tag => {
-				const type = tag.getAttribute("tag-type")
-				const tagName = tag.textContent.toLowerCase()
-				if (type==="ingredients") return card._ingredients.find(i => i.ingredient.toLowerCase().includes(tagName))
-				if (type==="appareils") return card._appliances.find(a => a.toLowerCase().includes(tagName))
-				if (type==="ustensiles") return card._ustensils.find(u => u.toLowerCase().includes(tagName)) 
-			}).length === tags.length
-		})
+		for (const card of cards) {
+		
+			let isCardValid = true;
+			
+			for (const tag of tags) {
+				const type = tag.getAttribute("tag-type");
+				const tagName = tag.textContent.toLowerCase();
+		
+				if (type === "ingredients") {
+
+					let ingredientFound = false;
+					for (const i of card._ingredients) {
+						if (i.ingredient.toLowerCase().includes(tagName)) {
+							ingredientFound = true;
+							break;
+						}
+					}
+
+					if (!ingredientFound) {
+						isCardValid = false;
+						break;
+					}
+				}
+		
+				if (type === "appareils") {
+
+					let applianceFound = false;
+
+					for (const a of card._appliances) {
+						if (a.toLowerCase().includes(tagName)) {
+						applianceFound = true;
+						break;
+						}
+					}
+
+					if (!applianceFound) {
+						isCardValid = false;
+						break;
+					}
+				}
+		
+				if (type === "ustensiles") {
+					
+					let ustensilFound = false;
+
+					for (const u of card._ustensils) {
+						if (u.toLowerCase().includes(tagName)) {
+						ustensilFound = true;
+						break;
+						}
+					}
+
+					if (!ustensilFound) {
+						isCardValid = false;
+						break;
+					}
+				}
+			}
+
+			if (isCardValid) {
+				filteredCards.push(card);
+			}
+		}
+
+		return filteredCards;
+
 	}
-
 }
 
 export default Tags
